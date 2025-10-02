110a371e8d2d9814bae43b56ab24f40109c392cf
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-10-02 06:48:19 +0600

    update js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-         
+const Base_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const dropDown = document.querySelectorAll(".dropDown select");
+
+for (let select of dropDown) {
+    for (let currCode in countryList) {
+        console.log(countryList);
+        
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        select.append(newOption);
+    }
+}
