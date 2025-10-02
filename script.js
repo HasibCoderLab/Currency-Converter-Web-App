84ee8b23ac49fb0db6dacac153efd8b91a042f1a
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-10-02 06:56:46 +0600

    update 'selected'

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ for (let select of dropDown) {
         let newOption = document.createElement("option");
         newOption.innerText = currCode;
         newOption.value = currCode;
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = "selected"
+        }else if (select.name === "to" && currCode === "BDT") {
+            newOption.selected = "selected"
+        }
         select.append(newOption);
     }
 }
